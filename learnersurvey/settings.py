0be76c5fdfa84277abfad69c710389801eade50f
qbook <clydewarden@gmail.com>
2024-04-14 21:18:14 +0800

    MIME add static path

--- learnersurvey/settings.py
+++ learnersurvey/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
+{% load static %}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
